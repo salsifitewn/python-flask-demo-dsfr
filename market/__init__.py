f10d816056796e4c0acd6d2c60862c2f196f29ad
salsifitewn <salsifite@gmail.com>
2021-10-05 00:52:53 +0200

    build: :hammer: add migration

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,11 @@
+import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL','sqlite:///market.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 from market import routes
